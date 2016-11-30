c2e12e16bc209d1ff3bde634277f8cc51d3335ef
Johannes Waldorf <johannes@amida-tech.com>
2016-11-29 21:39:53 -0500

    Color input

--- mastermind.py
+++ mastermind.py
@@ -1,5 +1,5 @@
 #author Gabe & JOJO
-
+import time
 
 def print_board(board):
     print ("This is the board")
@@ -11,18 +11,20 @@ def print_board(board):
                 print ("| ", end="")
 
             #print the respective color
-            if board[0][0] == "white":
-                print('  ' + '\x1b[6;30;42m' + '   ' + '\x1b[0m' + '  ', end="")   #end="prevents line break"
-            if board[0][0] == "white":
+            if board[position][row] == "blank":
+                print('       ', end="")
+            if board[position][row] == "cyan":
+                print('  ' + '\x1b[6;30;46m' + '   ' + '\x1b[0m' + '  ', end="")   #end="prevents line break"
+            if board[position][row] == "purple":
+                print('  ' + '\x1b[0;31;45m' + '   ' + '\x1b[0m' + '  ', end="")
+            if board[position][row] == "red":
+                print('  ' + '\x1b[6;30;41m' + '   ' + '\x1b[0m' + '  ', end="")
+            if board[position][row] == "green":
                 print('  ' + '\x1b[6;30;42m' + '   ' + '\x1b[0m' + '  ', end="")
-            if board[0][0] == "white":
-                print('  ' + '\x1b[6;30;42m' + '   ' + '\x1b[0m' + '  ', end="")
-            if board[0][0] == "white":
-                print('  ' + '\x1b[6;30;42m' + '   ' + '\x1b[0m' + '  ', end="")
-            if board[0][0] == "white":
-                print('  ' + '\x1b[6;30;42m' + '   ' + '\x1b[0m' + '  ', end="")
-            if board[0][0] == "white":
-                print('  ' + '\x1b[6;30;42m' + '   ' + '\x1b[0m' + '  ', end="")
+            if board[position][row] == "blue":
+                print('  ' + '\x1b[6;30;44m' + '   ' + '\x1b[0m' + '  ', end="")
+            if board[position][row] == "yellow":
+                print('  ' + '\x1b[6;30;43m' + '   ' + '\x1b[0m' + '  ', end="")
 
             #Everytime last position is reached print | at the end and line break
             if position == 3:
@@ -36,18 +38,42 @@ def initialize_board():
     return board
 
 def set_position(position,row,color):
-    board[position][row]= color
+    board[position][(11-row)]= color #11-row to set start printing at the bottom of the board
 
-
+def get_color(position, row):
+    color = (input('Enter color: ')).lower()
+    if color in colors: #check if input is a valid color
+        if colors.index(color)>5:
+            color = colors[(colors.index(color)-6)]
+        board[position][(11-row)]= color #11-row to set start printing at the bottom of the board
+        return True
+    else:
+        print ('\x1b[1A\x1b[2K\x1b[1A')
+        print ("Not a valid color, try again")
+        time.sleep(2)
+        print ('\x1b[1A\x1b[2K\x1b[1A')
+        return get_color(position, row)
 
 #set variable
-colors = ["grey","white","red","green","blue","yellow"]
+colors = ["cyan","purple","red","green","blue","yellow","c","p","r","g","b","y"]
 board = initialize_board()
-set_position(0,0,"white")
-set_position(3,11,"red")
-set_position(3,4,"green")
-set_position(0,1,"white")
+
 print_board(board)
+get_color(0,0)
+print_board(board)
+get_color(1,0)
+print_board(board)
+get_color(2,0)
+print_board(board)
+get_color(3,0)
+print_board(board)
+get_color(0,1)
+print_board(board)
+get_color(1,1)
+print_board(board)
+get_color(2,1)
+print_board(board)
+
 
 
 #initialize_board()
