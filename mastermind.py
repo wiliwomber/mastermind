571fe0b5a312b7ff4e1c42fc8922d5070ef78e15
Johannes Waldorf <johannes@amida-tech.com>
2016-12-02 23:30:39 -0500

    round function plus updates to scode generator

--- mastermind.py
+++ mastermind.py
@@ -48,6 +48,12 @@ def get_color(position, row):
             color = colors[(colors.index(color)-6)]
         board[position][(11-row)]= color #11-row to set start printing at the bottom of the board
         return color
+    elif (color == 'undo' and position > 0):
+            board[position-1][(11-row)]= 'blank'
+            print_board(board)
+            print("%d after blank"%position)
+            return 'blank'
+
     else:
         print ('\x1b[1A\x1b[2K\x1b[1A')
         print ("Not a valid color, try again")
@@ -57,9 +63,46 @@ def get_color(position, row):
 
 def gen_secretcode():
     print ("This is the secret code")
-    secretcode = random.sample(colors,4) # picks 4 random values from the array 'colors'
+    secretcode = []
+    for position in range (4):
+        secretcode.append(colors[random.randint(0,5)]) # picks 4 random values from full color names in  array 'colors'
     print (secretcode)
     return secretcode
+
+def submit():
+    #print ('\x1b[1A\x1b[2K\x1b[1A\x1b[1A\x1b[2K\x1b[1A') #delete last two text rows
+    submit_input = (input("Do you wanna submit? (Yes / No): ")).lower()
+    if submit_input == 'yes':
+        return (True)
+    elif submit_input == 'no':
+        print ('\x1b[1A\x1b[2K\x1b[1A')
+        return (False)
+    else:
+        print ('\x1b[1A\x1b[2K\x1b[1A')
+        print ("Please enter yes or no, try again")
+        time.sleep(2)
+        print ('\x1b[1A\x1b[2K\x1b[1A')
+        return submit()
+
+
+
+def round(board, scode, position):
+
+    print_board(board)
+    while (position < 4):
+        if get_color(position, row) != 'blank':
+            position +=1
+            print_board(board)
+        else:
+            position -=1
+            print_board(board)
+
+        print ("%d after color"%position)
+    if not submit():
+        set_position(position-1,row,'blank')
+        return round (board, scode, 3)
+
+
 
 # def guess_vs_secretcode(color):
 #    if color in scode:
@@ -76,15 +119,19 @@ colors = ["cyan","purple","red","green","blue","yellow","c","p","r","g","b","y"]
 #secret solution pattern
 #hit list   + (1)  - (0.2)
 board = initialize_board()
-scode = gen_secretcode()
+scode = gen_secretcode()    #secret combination colors
+row = 0  #round number -> determins the row which is played in
+position = 0 #
+round(board, scode, position)
 
 
+'''
 for turn in range(12):
     pegpoints = 0
     for guess in range(4):
         print_board(board)
         get_color(guess,turn)
-
+'''
 
 
 
