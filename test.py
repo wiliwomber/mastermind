f20dd1d9be05b140225bfa45bdc7cdefc65d2516
Johannes Waldorf <johannes@amida-tech.com>
2017-10-16 16:32:05 +0200

    alpha version done

--- test.py
+++ test.py
@@ -1,12 +1,5 @@
-from tkinter import *
 
-root = Tk()
-root.geometry("300x200")
 
-navbar = Frame(root, bg="green", width=100)
-navbar.pack(anchor=W, fill=Y, expand=False, side=LEFT)  # <----
-
-content_frame = Frame(root, bg="orange")
-content_frame.pack(anchor=N, fill=BOTH, expand=True, side=LEFT )
-
-root.mainloop()
+test = input()
+if test == '\x0D':
+    print('enter')
